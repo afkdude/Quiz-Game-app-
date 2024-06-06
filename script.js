8e3125ccff4bc1a4406d830660067f0f1138df4d
afkdude <kumarjordy12@gmail.com>
2024-06-06 23:27:53 +0530

    completed

--- script.js
+++ script.js
@@ -1,107 +1,164 @@
-const questionObj =
-{
-  category: 'Food & Drink',
-  id: 'qa-1',
-  correctAnswer: 'Three',
-  options: ['Two', 'Three', 'Four', 'Five'],
-  question:
-    "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+// const questionObj =
+// {
+//   category: 'Food & Drink',
+//   id: 'qa-1',
+//   correctAnswer: 'Three',
+//   options: ['Two', 'Three', 'Four', 'Five'],
+//   question:
+//     "How many pieces of bun are in a Mcdonald's Big Mac?",
+// };
+
+
+const quesJSON = [
+  {
+    correctAnswer: 'Three ',
+    options: ['Two', 'Three ', 'Four', 'Five'],
+    question:
+      "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: 'L. Frank Baum',
+    options: [
+      'Suzanne Collins',
+      'James Fenimore Cooper',
+      'L. Frank Baum',
+      'Donna Leon',
+    ],
+    question:
+      "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: 'Atlanta United',
+    options: [
+      'Atlanta United',
+      'Atlanta Impact',
+      'Atlanta Bulls',
+      'Atlanta Stars',
+    ],
+    question:
+      'Which of these is a soccer team based in Atlanta?',
+  },
+  {
+    correctAnswer: 'A Nanny',
+    options: [
+      'A Sow',
+      'A Lioness',
+      'A Hen',
+      'A Nanny',
+    ],
+    question: 'A female goat is known as what?',
+  },
+  {
+    correctAnswer: 'P. L. Travers',
+    options: [
+      'J. R. R. Tolkien',
+      'P. L. Travers',
+      'Lewis Carroll',
+      'Enid Blyton',
+    ],
+    question:
+      "Which author wrote 'Mary Poppins'?",
+  },
+];
+
 
 let score = 0; 
+let currentQuestion = 0; 
+const totalScore = quesJSON.length; 
 
+const questionEl = document.getElementById('question'); 
 const scoreEl = document.querySelector("#score");
+const optionsEl = document.getElementById('options'); 
+
+
+const nextEl = document.querySelector("#next"); 
+
 
 const scoreCount = document.createElement('h3'); 
 
 scoreEl.append(scoreCount); 
 
 
+showQuestion();
 
-
-// const questionEl = document.querySelector("#question");
-
-// questionEl.textContent = "Q1 "+questionObj.question;
-
-
-//destructuring
-
-const { correctAnswer, options, question } = questionObj; 
-
-
-const questionEl = document.getElementById('question'); 
-
-questionEl.textContent = question; 
-
-//getting access to the option section and then updating the buttons text using options in question obj
-
-
-
-const optionsEl = document.getElementById('options'); 
-
-// const buttonEl = document.createElement("button");
-
-// buttonEl.textContent = options[0];
-
-// optionsEl.appendChild(buttonEl);
-
-//approach 1
-// for (i = 0; i < options.length; i++){
-//   const buttonEl = document.createElement("button");
-//   buttonEl.textContent = options[i];
-//   optionsEl.append(buttonEl);
-// }
-
-
-
-//fetching shuffled array of options
-const shuffleOptions = shuffledOptions(options);
-//aproach 2 using foreach loop
-
-
-
-shuffleOptions.forEach((opt) => {
-  const buttonEl = document.createElement("button");
-  buttonEl.textContent = opt;
-  optionsEl.append(buttonEl)
-    ;
+nextEl.addEventListener('click', () => {
   
-
-  //event handling of buttons
-
-  buttonEl.addEventListener("click", ()=> {
-    if (opt === correctAnswer) {
-      score++;
-
-      //making the background color of correct option green 
-      buttonEl.style.backgroundColor = '#7BC74D';
-      buttonEl.style.color = "white"; 
-      
-    } else {
-      score = score - 0.25;
-
-      //making the background color of correct option red
-      buttonEl.style.backgroundColor = '#CD3F3E'; 
-      buttonEl.style.color = "white"; 
-
-    }
+  scoreCount.textContent = `Score : ${score} /${totalScore}`;
 
 
-    console.log(score);
 
-   //handling score display 
-    scoreCount.textContent = `Score : ${score}`; 
+  nextQuestion(); 
+});
 
-    //updating
+function showQuestion() {
+  //destructuring
 
-    questionEl.textContent = "Quiz Samapt!! "
-    optionsEl.textContent=""
+  const { correctAnswer, options, question } = quesJSON[currentQuestion];
+
+  //setting question text content 
+  questionEl.textContent = question;
+
+  const shuffleOptions = shuffledOptions(options);
+
+  shuffleOptions.forEach((opt) => {
+    const buttonEl = document.createElement("button");
+    buttonEl.textContent = opt;
+    optionsEl.append(buttonEl)
+      ;
+
+
+    //event handling of buttons
+
+    buttonEl.addEventListener("click", () => {
+      if (opt === correctAnswer) {
+        score++;
+
+        //making the background color of correct option green 
+        buttonEl.style.backgroundColor = '#7BC74D';
+        buttonEl.style.color = "white";
+
+      } else {
+        score = score - 0.25;
+
+        //making the background color of correct option red
+        buttonEl.style.backgroundColor = '#CD3F3E';
+        buttonEl.style.color = "white";
+
+      }
+
+
+      // console.log(score);
+
+      //handling score display 
+      scoreCount.textContent = `Score : ${score} /${totalScore}`;
+      nextQuestion(); 
+
+      //updating
+
+      // questionEl.textContent = "Quiz Samapt!! "
+      // optionsEl.textContent = ""
+
+    });
+
 
   });
+}
 
-  
-}); 
 
+
+function nextQuestion() {
+  currentQuestion++; 
+  // console.log("Current question ="+currentQuestion);
+  optionsEl.textContent = "";
+  if (currentQuestion >= quesJSON.length) {
+
+    questionEl.textContent = "Quiz Samapt!! "
+    nextEl.remove();
+    
+  } else {
+    showQuestion(); 
+  }
+}
 //shuffling the options
 
 
